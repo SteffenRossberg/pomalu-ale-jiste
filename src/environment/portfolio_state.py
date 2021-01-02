cf898671253b33a30714f81d4aa4b3c7671d4e5b
Steffen Roßberg <srossberg@gmx.net>
2021-01-02 11:34:01 +0100

    fixed issues in reward system, since net learns poorly

--- src/environment/portfolio_state.py
+++ src/environment/portfolio_state.py
@@ -88,25 +88,25 @@ class PortfolioState:
         price = self._frame['prices'][self._offset]
         count = int((self._investment - self.trading_fees) / price)
         if action == Actions.Buy and self._stock_count == 0 and count > 0:
-            returns = ((price / self._top_price) - 1.0) * -100.0
-            returns *= 1.0 - (self.tax_rate if returns > 0.0 else -1.0)
-            reward -= 100.0 * (self.trading_fees / (count * price))
+            returns = ((self._top_price / price) - 1.0)
+            returns *= 1.0 - (self.tax_rate if returns > 0.0 else 0.0)
+            reward -= (self.trading_fees / (count * price)) * 100.0
             if (self.__train_level & TrainingLevels.Buy) == TrainingLevels.Buy:
                 # try to find a good buy in point
-                reward += returns
+                reward += returns * 100.0
             self._investment -= self.trading_fees
             self._investment -= count * price
             self._stock_count = count
             self._buy_price = price
             self._sell_price = 0.0
-            self._top_price = 0.0
+            self._top_price = price
         elif action == Actions.Sell and self._stock_count > 0:
-            returns = ((price / self._buy_price) - 1.0) * 100.0
-            returns *= 1.0 - (self.tax_rate if returns > 0.0 else -1.0)
-            reward -= 100.0 * (self.trading_fees / (self._stock_count * price))
+            returns = ((price / self._buy_price) - 1.0)
+            returns *= 1.0 - (self.tax_rate if returns > 0.0 else 0.0)
+            reward -= (self.trading_fees / (self._stock_count * price)) * 100.0
             if (self.__train_level & TrainingLevels.Sell) == TrainingLevels.Sell:
                 # try to find a good sell out point
-                reward += returns
+                reward += returns * 100.0
             self._investment += self._buy_price * self._stock_count
             self._investment += returns
             self._stock_count = 0
@@ -117,16 +117,18 @@ class PortfolioState:
         elif action == Actions.SkipOrHold and self._stock_count == 0:
             if (self.__train_level & TrainingLevels.Skip) == TrainingLevels.Skip:
                 # try to find a good buy in point by skipping
-                imaginary_returns = ((price / self._top_price) - 1.0) * -100.0
+                imaginary_returns = ((price / self._top_price) - 1.0)
                 imaginary_returns *= 1.0 - (self.tax_rate if imaginary_returns > 0.0 else 0.0)
-                reward += imaginary_returns
+                if imaginary_returns > 0.0:
+                    reward += imaginary_returns * 100.0
             self._top_price = price if self._top_price < price else self._top_price
         elif action == Actions.SkipOrHold and self._stock_count > 0:
             if (self.__train_level & TrainingLevels.Hold) == TrainingLevels.Hold:
                 # try to find a good sell out point by holding
-                imaginary_returns = ((price / self._buy_price) - 1.0) * 100.0
+                imaginary_returns = ((price / self._buy_price) - 1.0)
                 imaginary_returns *= 1.0 - (self.tax_rate if imaginary_returns > 0.0 else 0.0)
-                reward += imaginary_returns
+                if imaginary_returns > 0.0:
+                    reward += imaginary_returns * 100.0
         self._offset += 1
         done |= self._offset >= len(self._frame['windows']) - self._days
         return reward, done
