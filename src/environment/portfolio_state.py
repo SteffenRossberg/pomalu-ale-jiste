5c1e66e3a6514cf4ff046faf5416eceaeef5ccc4
Steffen Roßberg <srossberg@gmx.net>
2021-01-02 21:45:25 +0100

    changes to reward system

--- src/environment/portfolio_state.py
+++ src/environment/portfolio_state.py
@@ -19,8 +19,8 @@ class PortfolioState:
         self._offset = None
         self._investment = self._start_investment
         self._stock_count = 0
+        self._trade_days = 0
         self._buy_price = 0.0
-        self._sell_price = 0.0
         self._top_price = 0.0
         self.__train_level = TrainingLevels.Buy
 
@@ -79,7 +79,6 @@ class PortfolioState:
         self._offset = offset
         self._stock_count = 0
         self._buy_price = 0.0
-        self._sell_price = self._frame['prices'][self._offset]
         self._top_price = self._frame['prices'][self._offset]
 
     def step(self, action):
@@ -88,47 +87,43 @@ class PortfolioState:
         price = self._frame['prices'][self._offset]
         count = int((self._investment - self.trading_fees) / price)
         if action == Actions.Buy and self._stock_count == 0 and count > 0:
-            returns = ((self._top_price / price) - 1.0)
-            returns *= 1.0 - (self.tax_rate if returns > 0.0 else 0.0)
             reward -= (self.trading_fees / (count * price)) * 100.0
-            if (self.__train_level & TrainingLevels.Buy) == TrainingLevels.Buy:
-                # try to find a good buy in point
-                reward += returns * 100.0
+            if self.__train_level & TrainingLevels.Buy == TrainingLevels.Buy:
+                reward += self.calculate_reward(self._top_price, price)
             self._investment -= self.trading_fees
             self._investment -= count * price
             self._stock_count = count
             self._buy_price = price
-            self._sell_price = 0.0
             self._top_price = price
+            self._trade_days = 1
         elif action == Actions.Sell and self._stock_count > 0:
-            returns = ((price / self._buy_price) - 1.0)
-            returns *= 1.0 - (self.tax_rate if returns > 0.0 else 0.0)
             reward -= (self.trading_fees / (self._stock_count * price)) * 100.0
-            if (self.__train_level & TrainingLevels.Sell) == TrainingLevels.Sell:
-                # try to find a good sell out point
-                reward += returns * 100.0
-            self._investment += self._buy_price * self._stock_count
-            self._investment += returns
+            if self.__train_level & TrainingLevels.Sell == TrainingLevels.Sell:
+                reward += self.calculate_reward(price, self._buy_price, self._trade_days)
+            self._investment -= self.trading_fees
+            self._investment += count * price
+            self._investment -= ((count * price) - (count * self._buy_price)) * (self.tax_rate if reward > 0.0 else 1.0)
+            self._trade_days = 0
             self._stock_count = 0
             self._buy_price = 0.0
-            self._sell_price = price
             self._top_price = price
             done |= self.reset_on_close
         elif action == Actions.SkipOrHold and self._stock_count == 0:
-            if (self.__train_level & TrainingLevels.Skip) == TrainingLevels.Skip:
-                # try to find a good buy in point by skipping
-                imaginary_returns = ((price / self._top_price) - 1.0)
-                imaginary_returns *= 1.0 - (self.tax_rate if imaginary_returns > 0.0 else 0.0)
-                if imaginary_returns > 0.0:
-                    reward += imaginary_returns * 100.0
+            self._trade_days += 1
+            if self.__train_level & TrainingLevels.Skip == TrainingLevels.Skip:
+                reward += self.calculate_reward(price, self._top_price, 1)
             self._top_price = price if self._top_price < price else self._top_price
         elif action == Actions.SkipOrHold and self._stock_count > 0:
-            if (self.__train_level & TrainingLevels.Hold) == TrainingLevels.Hold:
-                # try to find a good sell out point by holding
-                imaginary_returns = ((price / self._buy_price) - 1.0)
-                imaginary_returns *= 1.0 - (self.tax_rate if imaginary_returns > 0.0 else 0.0)
-                if imaginary_returns > 0.0:
-                    reward += imaginary_returns * 100.0
+            self._trade_days += 1
+            if self.__train_level & TrainingLevels.Hold == TrainingLevels.Hold:
+                reward += self.calculate_reward(price, self._buy_price, 1)
         self._offset += 1
         done |= self._offset >= len(self._frame['windows']) - self._days
         return reward, done
+
+    def calculate_reward(self, price, lower_price, factor=2):
+        returns = ((price / lower_price) - 1.0)
+        returns *= 1.0 - (self.tax_rate if returns > 0.0 else 0.0)
+        returns *= factor
+        reward = returns * 100.0
+        return reward
