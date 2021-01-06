97e88ddaef3472fa344c50bccca09fd794cc2496
Steffen Roßberg <srossberg@gmx.net>
2021-01-06 10:26:30 +0100

    refactoring

--- src/trading/trader.py
+++ src/trading/trader.py
@@ -1,26 +1,166 @@
 import torch
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.utility.logger import Logger
 from src.environment.enums import Actions
 
 
 class Trader:
 
-    def __init__(self, capital, order_fee, capital_gains_tax, solidarity_surcharge, stock_exchange, device, days=5):
+    def __init__(
+            self,
+            stock_exchange,
+            decision_maker,
+            max_positions,
+            max_limit_positions,
+            all_quotes,
+            all_tickers,
+            start_date,
+            end_date,
+            capital,
+            order_fee,
+            capital_gains_tax,
+            solidarity_surcharge,
+            device,
+            days=5):
+        self.stock_exchange = stock_exchange
+        self.decision_maker = decision_maker
+        self.max_positions = max_positions
+        self.max_limit_positions = max_limit_positions
+        self.all_quotes = all_quotes
+        self.all_tickers = all_tickers
+        self.start_date = start_date
+        self.end_date = end_date
         self.start_capital = capital
         self.capital_gains_tax = capital_gains_tax
         self.solidarity_surcharge = solidarity_surcharge
-        self.stock_exchange = stock_exchange
         self.device = device
         self.days = days
         self.tax_rate = self.capital_gains_tax / 100.0
         self.tax_rate *= self.solidarity_surcharge / 100.0 + 1.0
         self.order_fee = order_fee
 
-    def buy_and_hold(self,
-                     quotes,
-                     all_tickers,
-                     report_each_trade=True,
-                     tickers=None):
+    def trade(
+            self,
+            run_id):
+
+        result = ''
+
+        print(f"Trade limited all stocks from {self.start_date} to {self.end_date} ...")
+        message, limit_all_investments, limit_all_gain_loss = \
+            self._trade(
+                self.decision_maker,
+                self.all_quotes,
+                self.all_tickers,
+                True,
+                self.stock_exchange.tickers,
+                max_positions=self.max_limit_positions)
+        result += f'\nTrade Portfolio (max {int(len(self.stock_exchange.tickers) / 3)} stocks): {message}'
+
+        print(f"Trade all stocks from {self.start_date} to {self.end_date} ...")
+        message, all_investments, all_gain_loss = \
+            self._trade(
+                self.decision_maker,
+                self.all_quotes,
+                self.all_tickers,
+                True,
+                self.stock_exchange.tickers,
+                max_positions=self.max_positions)
+        result += f'\nTrade All ({len(self.stock_exchange.tickers)} stocks): {message}'
+
+        print(f"Buy and hold all stocks from {self.start_date} to {self.end_date} ...")
+        message = \
+            self._buy_and_hold(
+                self.all_quotes,
+                self.all_tickers,
+                False,
+                self.stock_exchange.tickers)
+        result += f'\nBuy % Hold All ({len(self.stock_exchange.tickers)} stocks): {message}'
+
+        print(result)
+        Logger.log(run_id, result)
+
+        index_ticker = 'URTH'
+        index_title = self.stock_exchange.etf_tickers[index_ticker]
+        compare_index = self.stock_exchange.load(index_ticker, self.start_date, self.end_date, True)
+
+        all_title = f'All stocks ({self.max_positions} positions)'
+        limit_all_title = f'All stocks (max. {self.max_limit_positions} positions at once)'
+        gain_loss_all_title = f'Return all stocks ({self.max_positions} positions)'
+        gain_loss_limit_all_title = f'Return all stocks (max. {self.max_limit_positions} positions at once)'
+
+        length = (len(compare_index)
+                  if len(compare_index) < len(all_investments)
+                  else len(all_investments))
+
+        resulting_frame = pd.DataFrame(
+            data={
+                'index': range(length),
+                'date': np.array(compare_index['date'].values[-length:]),
+                index_title: np.array(compare_index['close'].values[-length:]),
+                all_title: np.array(all_investments[-length:]),
+                limit_all_title: np.array(limit_all_investments[-length:]),
+                gain_loss_all_title: np.array(all_gain_loss[-length:]) + self.start_capital,
+                gain_loss_limit_all_title: np.array(limit_all_gain_loss[-length:]) + self.start_capital
+            })
+
+        all_columns = [
+            index_title,
+            all_title,
+            limit_all_title,
+            gain_loss_all_title,
+            gain_loss_limit_all_title
+        ]
+        for column in all_columns:
+            change_column = f'Change {column}'
+            resulting_frame[change_column] = resulting_frame[column].pct_change(1).fillna(0.0) * 100.0
+            resulting_frame[column] = \
+                resulting_frame.apply(
+                    lambda row: np.sum(resulting_frame[change_column].values[0:int(row['index'] + 1)]),
+                    axis=1)
+
+        resulting_frame.set_index(resulting_frame['date'], inplace=True)
+
+        fig, ax = plt.subplots(nrows=2)
+
+        investment_columns = [
+            all_title,
+            limit_all_title
+        ]
+        resulting_frame[index_title].plot.area(ax=ax[0], stacked=False)
+        resulting_frame[investment_columns].plot(
+            ax=ax[0],
+            figsize=(20, 10),
+            linewidth=2,
+            colormap='Spectral',
+            title=f'Investment vs {index_title}')
+
+        gain_loss_columns = [
+            gain_loss_all_title,
+            gain_loss_limit_all_title
+        ]
+        resulting_frame[index_title].plot.area(ax=ax[1], stacked=False)
+        resulting_frame[gain_loss_columns].plot(
+            ax=ax[1],
+            figsize=(20, 10),
+            linewidth=2,
+            colormap='Spectral',
+            title=f'Portfolio Changes vs {index_title}')
+
+        results = resulting_frame[gain_loss_columns].copy()
+        results.to_csv(f'data/{run_id}.trading.gain_loss.csv')
+
+        plt.savefig(f'data/{run_id}.chart.png')
+        plt.show()
+        plt.close()
+
+    def _buy_and_hold(
+            self,
+            quotes,
+            all_tickers,
+            report_each_trade=True,
+            tickers=None):
         if tickers is None:
             tickers = self.stock_exchange.tickers
         tickers = {ticker: self.stock_exchange.tickers[ticker] for ticker in tickers.keys() if ticker in all_tickers}
@@ -58,13 +198,14 @@ class Trader:
         self.report(message, True)
         return message
 
-    def trade(self,
-              agent,
-              quotes,
-              all_tickers,
-              report_each_trade=True,
-              tickers=None,
-              max_positions=None):
+    def _trade(
+            self,
+            agent,
+            quotes,
+            all_tickers,
+            report_each_trade=True,
+            tickers=None,
+            max_positions=None):
         if tickers is None:
             tickers = self.stock_exchange.tickers
         tickers = {ticker: self.stock_exchange.tickers[ticker] for ticker in tickers.keys() if ticker in all_tickers}
@@ -84,7 +225,7 @@ class Trader:
                         portfolio[ticker]['last_price'] = row[f'{ticker}_close']
                 # for ticker in portfolio.keys():
                 #     position = portfolio[ticker]
-                #     if self.calculate_position_gain_loss(ticker, position, row) >= 5.0:
+                #     if self.calculate_position_gain_loss(ticker, position, row) >= 2.0:
                 #         if ticker in actions:
                 #             actions[ticker]['index'] = Actions.Sell
                 #         else:
@@ -162,7 +303,11 @@ class Trader:
 
         return message, investments, gain_loss
 
-    def calculate_position_gain_loss(self, ticker, position, row):
+    def calculate_position_gain_loss(
+            self,
+            ticker,
+            position,
+            row):
         price = row[f'{ticker}_close']
         if not price > 0.0:
             price = position['last_price']
@@ -178,7 +323,13 @@ class Trader:
         returns = ((gain_loss / buy_in) - 1.0) * 100.0
         return returns
 
-    def clear_positions(self, portfolio, row, investment, gain_loss, report_each_trade):
+    def clear_positions(
+            self,
+            portfolio,
+            row,
+            investment,
+            gain_loss,
+            report_each_trade):
         tickers = [ticker for ticker in portfolio.keys()]
         for ticker in tickers:
             position = portfolio[ticker]
@@ -201,7 +352,14 @@ class Trader:
             del portfolio[ticker]
         return investment, gain_loss
 
-    def calculate_actions(self, agent, tickers, portfolio, quotes, row, index):
+    def calculate_actions(
+            self,
+            agent,
+            tickers,
+            portfolio,
+            quotes,
+            row,
+            index):
         features, eval_tickers = self.calculate_features(tickers, portfolio, quotes, row, index)
         if eval_tickers is None:
             return None
@@ -218,7 +376,13 @@ class Trader:
         }
         return actions
 
-    def calculate_features(self, tickers, portfolio, quotes, row, index):
+    def calculate_features(
+            self,
+            tickers,
+            portfolio,
+            quotes,
+            row,
+            index):
         evaluated_tickers = []
         feature_batch = []
         for ticker in tickers.keys():
@@ -244,7 +408,11 @@ class Trader:
         feature_batch = feature_batch.to(self.device)
         return feature_batch, evaluated_tickers
 
-    def calculate_current_investment(self, investment, portfolio, row):
+    def calculate_current_investment(
+            self,
+            investment,
+            portfolio,
+            row):
         for ticker, position in portfolio.items():
             price = row[f'{ticker}_close']
             if not price > 0.0:
@@ -259,7 +427,9 @@ class Trader:
         return investment
 
     @staticmethod
-    def report(message, verbose):
+    def report(
+            message,
+            verbose):
         if not verbose:
             return
         print(message)
