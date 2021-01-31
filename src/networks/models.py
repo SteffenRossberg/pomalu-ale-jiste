b729fa30a2ff4d37406f7ea8e0de008348fde054
Steffen Roßberg <srossberg@gmx.net>
2021-01-31 13:50:20 +0100

    init weights and use view rather than resharp

--- src/networks/models.py
+++ src/networks/models.py
@@ -16,7 +16,7 @@ class Trader(nn.Module):
 
     def forward(self, features):
         encoder_features = features[:, :features.shape[1] - self.state_size]
-        encoder_features = encoder_features.reshape(features.shape[0], 1, self.days, 4)
+        encoder_features = encoder_features.view((features.shape[0], 1, self.days, 4))
         encoder_features = encoder_features.to(features.device)
 
         buyer_prediction = self.buyer(encoder_features)
@@ -27,7 +27,7 @@ class Trader(nn.Module):
         detector_features = self.__merge_mse(buyer_mse, seller_mse)
 
         state_features = features[:, -self.state_size:]
-        state_features = state_features.reshape(features.shape[0], 1, 1, self.state_size)
+        state_features = state_features.view((features.shape[0], 1, 1, self.state_size))
         state_features = state_features.to(features.device)
         decision_features = self.__merge_state(detector_features, state_features)
 
@@ -58,14 +58,14 @@ class Trader(nn.Module):
         diff = x - y
         diff = torch.squeeze(diff, dim=0)
         square = diff * diff
-        square = square.reshape(square.shape[0], 1, square.shape[-2], square.shape[-1])
+        square = square.view((square.shape[0], 1, square.shape[-2], square.shape[-1]))
         mse = torch.mean(square, dim=(2, 3))
         return mse
 
     @staticmethod
     def __merge_mse(buyer_mse, seller_mse):
         seller_shape = seller_mse.shape
-        seller_reshaped = seller_mse.reshape(seller_shape[1], seller_shape[0])
+        seller_reshaped = seller_mse.view((seller_shape[1], seller_shape[0]))
         merged_mse = torch.cat((buyer_mse, torch.t(seller_reshaped)), 1)
         return merged_mse
 
@@ -73,13 +73,13 @@ class Trader(nn.Module):
         prepared_features = self.__prepare_data(features)
         prepared_state = self.__prepare_data(state)
         merged_features = torch.cat((prepared_features, prepared_state), 1)
-        result = merged_features.reshape(features.shape[0], 1, 1, merged_features.shape[-1])
+        result = merged_features.view((features.shape[0], 1, 1, merged_features.shape[-1]))
         return result
 
     @staticmethod
     def __prepare_data(data):
         data_shape = data.shape
-        prepared_data = data.reshape(data_shape[0], data_shape[-1])
+        prepared_data = data.view((data_shape[0], data_shape[-1]))
         return prepared_data
 
 
@@ -89,29 +89,47 @@ class DecisionMaker(nn.Module):
         self.state_size = state_size
         in_features = 2 + state_size
         self.adv = nn.Sequential(
-            nn.Linear(in_features=in_features, out_features=512),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=in_features,
+                    out_features=512)),
             nn.Dropout(0.2),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=512),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=512,
+                    out_features=512)),
             nn.Dropout(0.2),
-            nn.Sigmoid(),
-            nn.Linear(in_features=512, out_features=3),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=512,
+                    out_features=3)),
         )
         self.val = nn.Sequential(
-            nn.Linear(in_features=in_features, out_features=512),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=in_features,
+                    out_features=512)),
             nn.Dropout(0.2),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=512),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=512,
+                    out_features=512)),
             nn.Dropout(0.2),
-            nn.Sigmoid(),
-            nn.Linear(in_features=512, out_features=1),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=512,
+                    out_features=1)),
         )
 
     def forward(self, features):
         val = self.val(features)
         adv = self.adv(features)
         observation = val + adv - adv.mean(dim=3, keepdim=True)
-        observation = observation.reshape(features.shape[0], 3)
+        observation = observation.view((features.shape[0], 3))
         return observation
 
     def _forward_unimplemented(self, *features: Any) -> None:
@@ -148,12 +166,18 @@ class Encoder(nn.Module):
         layers = OrderedDict()
         for i in range(conv_layer_count):
             key = f'layer{i}'
-            layers[key] = nn.Conv2d(self.in_channels, self.out_channels, kernel, stride, padding)
+            layers[key] = NetHelper.init_weights(
+                nn.Conv2d(
+                    self.in_channels,
+                    self.out_channels,
+                    kernel,
+                    stride,
+                    padding))
             self.shape = ConvolutionHelper.calc_2d_size(self.shape, kernel, stride, padding)
             layers[key + '_pool'] = nn.MaxPool2d(kernel_size=kernel, padding=(1, 0), stride=(1, 1))
             self.shape = ConvolutionHelper.calc_2d_size(self.shape, kernel, padding=(1, 0), stride=(1, 1))
             layers[key + '_drop_out'] = nn.Dropout2d(0.2)
-            layers[key + '_activation'] = nn.ReLU()
+            layers[key + '_activation'] = nn.LeakyReLU()
             self.in_channels = self.out_channels
             self.out_channels += int(self.out_channels / 4)
         self.out_channels = self.in_channels
@@ -161,17 +185,23 @@ class Encoder(nn.Module):
 
         self.convolution = nn.Sequential(layers)
         self.linear = nn.Sequential(
-            nn.Linear(in_features=self.conv_out_size, out_features=int(self.conv_out_size / 4)),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=self.conv_out_size,
+                    out_features=int(self.conv_out_size / 4))),
             nn.Dropout(0.2),
-            nn.Tanh(),
-            nn.Linear(in_features=int(self.conv_out_size / 4), out_features=output_size),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=int(self.conv_out_size / 4),
+                    out_features=output_size)),
             nn.Dropout(0.2),
-            nn.Tanh()
+            nn.LeakyReLU()
         )
 
     def forward(self, features):
         result = self.convolution(features)
-        result = result.reshape(features.size()[0], 1, 1, result.shape[-3] * result.shape[-2] * result.shape[-1])
+        result = result.view((features.size()[0], 1, 1, result.shape[-3] * result.shape[-2] * result.shape[-1]))
         result = self.linear(result)
         return result
 
@@ -201,26 +231,38 @@ class Decoder(nn.Module):
             key = f'layer{i - 1}'
             in_channel = channels[i]
             out_channel = channels[i - 1]
-            layers[key] = nn.ConvTranspose2d(in_channel, out_channel, kernel, stride, padding)
+            layers[key] = NetHelper.init_weights(
+                nn.ConvTranspose2d(
+                    in_channel,
+                    out_channel,
+                    kernel,
+                    stride,
+                    padding))
             self.shape = ConvolutionHelper.calc_2d_size(self.shape, kernel, stride, padding)
             layers[key + '_drop_out'] = nn.Dropout2d(0.2)
-            layers[key + '_activation'] = nn.ReLU()
+            layers[key + '_activation'] = nn.LeakyReLU()
 
         self.transpose = nn.Sequential(layers)
         self.out_channels = channels[-1]
 
         self.linear = nn.Sequential(
-            nn.Linear(in_features=self.input_size, out_features=128),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=self.input_size,
+                    out_features=128)),
             nn.Dropout(0.2),
-            nn.Tanh(),
-            nn.Linear(in_features=128, out_features=self.out_channels * self.shape[0] * self.shape[1]),
+            nn.LeakyReLU(),
+            NetHelper.init_weights(
+                nn.Linear(
+                    in_features=128,
+                    out_features=self.out_channels * self.shape[0] * self.shape[1])),
             nn.Dropout(0.2),
-            nn.Tanh()
+            nn.LeakyReLU()
         )
 
     def forward(self, features):
         result = self.linear(features)
-        result = result.reshape(features.size()[0], self.out_channels, self.shape[0], self.shape[1])
+        result = result.view((features.size()[0], self.out_channels, self.shape[0], self.shape[1]))
         result = self.transpose(result)
         return result
 
@@ -241,3 +283,10 @@ class ConvolutionHelper:
         padding *= 2
         kernel = dilation * (kernel - 1)
         return int(((size + padding - kernel - 1) / stride) + 1)
+
+
+class NetHelper:
+    @staticmethod
+    def init_weights(layer, mean=0.0, std=0.05):
+        torch.nn.init.normal_(layer.weight, mean=mean, std=std)
+        return layer
